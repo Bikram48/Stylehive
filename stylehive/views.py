3dbc24048f679913b4c862e4ccde8ef96d4ef1d1
Bikram48 <chandbikram001@gmail.com>
2023-03-11 18:40:31 +0545

    make - login and signup feature working

--- stylehive/views.py
+++ stylehive/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 
 def home(request):
     return HttpResponse("Home")
 
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            print("User doesn't exist")
+      
     return render(request, "login.html", {})
 
 def user_registration(request):
-    return render(request, "register.html", {})
+
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "register.html", context)
